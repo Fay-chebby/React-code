9f6fd58cb2fe65827e727279de7b1536381b7b6c
Fai-chebby <chebetf034@gmail.com>
2025-02-11 11:11:31 +0300

    styling

--- components/src/components/Login page.jsx
+++ components/src/components/Login page.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
-
+import "./Login.css"
 const   Login =()=>{
     return(
         <div className="login">
+            <div className="background-circles">
+                <div className="circle circle1"></div>
+                <div className="circle circle2"></div>
+                <div className="circle circle3"></div>
+            </div>
             <h1 className="header"> Login</h1>
             <form className="login-form">
                 <div className="form-group">
                     <label htmlFor="email">Email</label>
-                    <input type="email" className="form-control" id="email" placeholder="Email" />
+                    <input type="email" className="form-control" id="email" placeholder="Email"/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input type="password" className="form-control" id="password" placeholder="Password" />
+                    <input type="password" className="form-control" id="password" placeholder="Password"/>
                 </div>
                 <button type="submit">Login</button>
                 <div className="register link">
